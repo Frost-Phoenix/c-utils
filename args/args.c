971961f4fb1eaf3a0a248b4b556afbf42b520038
Frost-Phoenix <67cyril6767@gmail.com>
2024-04-12 17:05:06 +0200

    add error func

--- args/args.c
+++ args/args.c
@@ -11,14 +11,39 @@
 #define OPT_NON_OPTION_SEPARATOR  4
 
 
+enum error_type {
+    ERROR_NULL_POINTER,
+    ERROR_WRONG_OPTION_TYPE,
+    ERROR_OPT_SHORT,
+    ERROR_OPT_LONG,
+};
+
+
 /******************************************************
  *                 Private functions                  *
  ******************************************************/
+
+static void priv_error(args_t* args, const args_option_t* option, const char* reason, enum error_type cause) {
+    if (cause == ERROR_NULL_POINTER) {
+        fprintf(stderr, "\033[31merror:\033[0m option value is a NULL pointer\n");
+    } else if (cause == ERROR_WRONG_OPTION_TYPE) {
+        fprintf(stderr, "\033[31merror:\033[0m wrong option type: %d\n", option->type);
+    } else if (ERROR_OPT_SHORT) {
+        fprintf(stderr, "\033[31merror:\033[0m option `-%c` %s\n", option->short_name, reason);
+    } else if (ERROR_OPT_LONG) {
+        fprintf(stderr, "\033[31merror:\033[0m option `--%s` %s\n", option->long_name, reason);
+    }
+    printf("Try '%s --help' for more information.\n", args->name);
+    exit(EXIT_FAILURE);
+}
 
 static void priv_check_options(args_t* args) {
     const args_option_t* options = args->options;
 
     for (;options->type != OPT_END; options++) {
+        if (options->short_name != 'h' && options->val == NULL) {
+            priv_error(args, options, NULL, ERROR_NULL_POINTER);
+        }
         switch (options->type) {
             case OPT_BOOL:
             case OPT_INT:
@@ -26,9 +51,7 @@ static void priv_check_options(args_t* args) {
             case OPT_HELP:
                 continue;
             default:
-                fprintf(stderr, "\033[31merror:\033[0m wrong option type: %d\n", options->type);
-                printf("Try '%s --help' for more information.\n", args->name);
-                exit(EXIT_FAILURE);
+                priv_error(args, options, NULL, ERROR_WRONG_OPTION_TYPE);
         }
     }
 }
@@ -96,8 +119,6 @@ void args_parse(args_t* args) {
         const char* opt = args->argv[0];
 
         switch (priv_get_opt_type(opt)) {
-            case OPT_NON_OPTION:
-                break;
             case OPT_SHORT:
                 opt++;
                 while (*opt) {
@@ -106,6 +127,10 @@ void args_parse(args_t* args) {
                 }
                 break;
             case OPT_LONG:
+                break;
+            case OPT_NON_OPTION:
+                break;
+            case OPT_NON_OPTION_SEPARATOR:
                 break;
             default:
                 break;
